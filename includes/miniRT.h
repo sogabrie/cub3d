a4f203f55dc0572b06d94eb8494c3fb7ca8d273c
ZoktrFall <shahavetgnum@gmail.com>
2023-05-30 22:42:39 +0400

    (defolt home) working pars part

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -13,10 +13,12 @@
 # include "mlx.h"
 
 //  Error and Warning messages
+# define E_MALLOC "Error\n! Failed to allocate dynamic memory !\n"
 # define E_ARGC_1 "Error\n! Please specify the .rt file !\n"
 # define W_ARGC_2 "Warning\n! Only the first argument is considered !\n"
 # define E_OPEN_FILE "Error\n! It was not possible to open the file !\n"
 # define E_FILE_EMPTY "Error\n! .rt file is empty !\n"
+# define E_CONFIGURATION "Error\n! wrong file configuration !\n"
 
 //  Pars Part
 int	open_file(char *file_name);
--- src/main/main.c
+++ src/main/main.c
@@ -1,37 +1,45 @@
 #include "miniRT.h"
 
-// void	permissible_parameters(char *file_name)
-// {
-// 	int		fd;
-// 	char	*str;
-// 	int		f_empty;
+char	**creat_mas_rt(char **file_rt, char *file_name)
+{
+	int		fd;
+	char	*tmp_str;
+	char	*tmp;
 
-// 	f_empty = 0;
-// 	fd = open_file(file_name);
-// 	while (1)
-// 	{
-// 		str = get_next_line(fd);
-// 		if (!str)
-// 			break ;
-// 		if (str[0] == '\n')
-// 		{
-// 			free(str);
-// 			continue ;
-// 		}
-// 		//check &f_empty = 1;
-// 		free(str);
-// 	}
-// 	if (!f_empty)
-// 		exit(printf(E_FILE_EMPTY));
-// }
+	fd = open_file(file_name);
+	tmp_str = NULL;
+	while (1)
+	{
+		tmp = get_next_line(fd);
+		if (tmp == NULL)
+			break ;
+		tmp_str = ft_strjoin_exlusive(tmp_str, tmp);
+		if (tmp_str == NULL)
+			exit(printf(E_MALLOC));
+		free(tmp);
+	}
+	close(fd);
+	if (tmp_str == NULL)
+		exit(printf(E_FILE_EMPTY));
+	file_rt = ft_split(tmp_str, '\n');
+	if (file_rt == NULL)
+		exit(printf(E_MALLOC));
+	free(tmp_str);
+	return (file_rt);
+}
 
 int	parsing_file(int argc, char *argv)
 {
+	char	**file_rt;
+
+	file_rt = NULL;
 	if (argc == 1 || check_rt(argv, ".rt"))
 		return (printf(E_ARGC_1));
 	if (argc > 2)
 		printf(W_ARGC_2);
-	// permissible_parameters(argv);
+	file_rt = creat_mas_rt(file_rt, argv);
+	for(int i = 0; file_rt[i]; i++)
+		printf("%s\n", file_rt[i]);
 	return (0);
 }
 
