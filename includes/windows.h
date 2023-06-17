f76467003e5f3a587ad32d17ba08a3208c5aa7e9
sogabrie <gabrielyan.sos@mail.ru>
2023-06-18 03:02:29 +0400

    update

--- includes/windows.h
+++ includes/windows.h
@@ -12,14 +12,55 @@
 #include "cub3D.h"
 #include "def_struct.h"
 
+typedef struct s_map
+{
+	int		*no;
+	int		*so;
+	int		*we;
+	int		*ea;
+	int		x;
+	int		y;
+}				t_map;
+
+typedef struct s_point
+{
+	double	x;
+	double	y;
+}				t_point;
+
+typedef struct s_player {
+	t_point	pos;
+	t_point	dir;
+}				t_player;
+
+typedef struct s_ray
+{
+	t_point	dir;
+	t_point	delta;
+	t_point	side;
+	int		i;
+	int		step_x;
+	int		step_y;
+	int		vert_hit;
+}				t_ray;
+
 typedef struct s_data
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
 }				t_data;
+
+typedef struct s_wall
+{
+	int		height;
+	int		top;
+	int		tex_x;
+	double	tex_y;
+	double	dist;
+}				t_wall;
 
 typedef struct s_param
 {
@@ -27,8 +68,14 @@ typedef struct s_param
 	void			*win_ptr;
 	t_data			img;
     t_colors		col;
+	t_point			wind;
+	t_map			map;
 	double			x;
 	double			y;
+	t_wall			wall;
+	t_ray			ray;
+	t_player		plr;
+	t_point			plane;
 	t_data_segment	*data;
 }					t_param;
 
--- src/windows/windows.c
+++ src/windows/windows.c
@@ -40,7 +40,7 @@ int	deal_key(int key, t_param *ptr)
 }
 
 
-void	ft_create(t_param *ptr, int color, int start)
+void	create_c_f(t_param *ptr, int color, int start)
 {
 	int	i;
 	int	k;
@@ -58,10 +58,139 @@ void	ft_create(t_param *ptr, int color, int start)
 	}
 }
 
+///
+void	initcalizac(t_param *ptr)
+{
+	double	camera_x;
+
+	camera_x = ptr->ray.i * 2.0 / (double)ptr->x - 1.0;
+	ptr->ray.dir.x = ptr->plr.dir.x + ptr->plane.x * camera_x;
+	ptr->ray.dir.y = ptr->plr.dir.y + ptr->plane.y * camera_x;
+	ptr->map.x = (int)ptr->plr.pos.x;
+	ptr->map.y = (int)ptr->plr.pos.y;
+	ptr->ray.delta.x = fabs(1.0 / ptr->ray.dir.x);
+	ptr->ray.delta.y = fabs(1.0 / ptr->ray.dir.y);
+	ptr->ray.step_x = 1;
+	ptr->ray.side.x = (ptr->map.x + 1.0 - ptr->plr.pos.x) * ptr->ray.delta.x;
+	ptr->ray.step_y = 1;
+	ptr->ray.side.y = (ptr->map.y + 1.0 - ptr->plr.pos.y) * ptr->ray.delta.y;
+	if (ptr->ray.dir.x < 0)
+	{
+		ptr->ray.step_x = -1;
+		ptr->ray.side.x = (ptr->plr.pos.x - ptr->map.x) * ptr->ray.delta.x;
+	}
+	if (ptr->ray.dir.y < 0)
+	{
+		ptr->ray.step_y = -1;
+		ptr->ray.side.y = (ptr->plr.pos.y - ptr->map.y) * ptr->ray.delta.y;
+	}
+}
+
+void run(t_param *ptr)
+{
+	int	collision;
+
+	collision = 0;
+	while (!collision)
+	{
+		if (ptr->ray.side.x < ptr->ray.side.y)
+		{
+			ptr->ray.side.x += ptr->ray.delta.x;
+			ptr->map.x += ptr->ray.step_x;
+			ptr->ray.vert_hit = 0;
+		}
+		else
+		{
+			ptr->ray.side.y += ptr->ray.delta.y;
+			ptr->map.y += ptr->ray.step_y;
+			ptr->ray.vert_hit = 1;
+		}
+		if (ptr->data->map[(int)ptr->map.y][(int)ptr->map.x] == '1')
+			collision = 1;
+	}
+}
+
+void	calc_wall(t_param *ptr)
+{
+	if (!ptr->ray.vert_hit)
+		ptr->wall.dist = ptr->ray.side.x - ptr->ray.delta.x;
+	else
+		ptr->wall.dist = ptr->ray.side.y - ptr->ray.delta.y;
+	ptr->wall.height = (int)(ptr->y / ptr->wall.dist);
+	ptr->wall.top = -ptr->wall.height / 2 + ptr->y / 2;
+}
+
+void	find_tex_x(t_param *ptr)
+{
+	double	wall_x;
+
+	if (!ptr->ray.vert_hit)
+		wall_x = ptr->plr.pos.y + ptr->wall.dist * ptr->ray.dir.y;
+	else
+		wall_x = ptr->plr.pos.x + ptr->wall.dist * ptr->ray.dir.x;
+	wall_x -= floor(wall_x);
+	ptr->wall.tex_x = (int)(wall_x * 64.0);
+	if ((!ptr->ray.vert_hit && ptr->ray.dir.x > 0)
+		|| (ptr->ray.vert_hit && ptr->ray.dir.y < 0))
+		ptr->wall.tex_x = 64 - ptr->wall.tex_x - 1;
+}
+
+int	find(t_param *ptr, int x, int y)
+{
+	int	*tex;
+
+	if (!ptr->ray.vert_hit)
+	{
+		if (ptr->ray.dir.x >= 0)
+			tex = ptr->map.ea;
+		else
+			tex = ptr->map.we;
+	}
+	else
+	{
+		if (ptr->ray.dir.y >= 0)
+			tex = ptr->map.so;
+		else
+			tex = ptr->map.no;
+	}
+	return (tex[64 * y + x]);
+}
+
+void	creat_line(t_param *ptr)
+{
+	double	scale;
+	int		color;
+	int		i;
+
+	scale = 64.0 / (double)ptr->wall.height;
+	ptr->wall.tex_y = 0;
+	i = 0;
+	while (i < ptr->wall.height)
+	{
+		color = find(ptr, ptr->wall.tex_x, (int)ptr->wall.tex_y);
+		my_mlx_pixel_put(&(ptr->img), ptr->ray.i, ptr->wall.top + i, color);
+		ptr->wall.tex_y += scale;
+		i++;
+	}
+}
+
+///
+
 int creat_wall(t_param *ptr)
 {
-	ft_create(ptr, create_trgb(1, ptr->data->options.ceiling.red, ptr->data->options.ceiling.green, ptr->data->options.ceiling.blue), 0);
-	ft_create(ptr, create_trgb(1, ptr->data->options.floor.red, ptr->data->options.floor.green, ptr->data->options.floor.blue), ptr->y / 2);
+	int	i = 0;
+	create_c_f(ptr, create_trgb(1, ptr->data->options.ceiling.red, ptr->data->options.ceiling.green, ptr->data->options.ceiling.blue), 0);
+	create_c_f(ptr, create_trgb(1, ptr->data->options.floor.red, ptr->data->options.floor.green, ptr->data->options.floor.blue), ptr->y / 2);
+
+	while (i < ptr->x)
+	{
+		initcalizac(ptr);
+		run(ptr);
+		calc_wall(ptr);
+		find_tex_x(ptr);
+		creat_line(ptr);
+		++i;
+	}
 
 	mlx_put_image_to_window(ptr->mlx_ptr, ptr->win_ptr, ptr->img.img, 0, 0);
 	return (0);
